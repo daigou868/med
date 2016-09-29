439eb032c08af13f6ddbb033a9142600bb923852
daigou868 <hello@868daigou.com>
2016-09-29 14:27:25 +0930

    Update and rename .gitignore to med.sh

---
+++ med.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+youtube-dl https://www.youtube.com/watch?v=Gqfk5sr9fpw
+youtube-dl https://www.youtube.com/watch?v=eM2_GJltT4Y
+youtube-dl https://www.youtube.com/watch?v=ytoMrm27WV0&list=PLM-03BGcCI9XTNXn5uBbYKt1RIRl_6L5N
